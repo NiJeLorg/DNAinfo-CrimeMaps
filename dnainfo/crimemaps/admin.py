05425ad906520ef0ce30f1749623e19c8f4bcd91
JD Godchaux <jd@nijel.org>
2015-05-01 16:58:31 -0400

    Updated compstat data

--- dnainfo/crimemaps/admin.py
+++ dnainfo/crimemaps/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from crimemaps.models import *
+
+admin.site.register(compstat)
+admin.site.register(doitt)
+admin.site.register(blotter)
