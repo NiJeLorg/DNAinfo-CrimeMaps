2fdfbc01a5c7dc59e52a5b0232870d24ebfdf835
JD Godchaux <jd@nijel.org>
2015-05-13 11:33:13 -0400

    and for doitt

--- dnainfo/crimemaps/static/crimemaps/js/DNAinfoDoittMap.js
+++ dnainfo/crimemaps/static/crimemaps/js/DNAinfoDoittMap.js
@@ -18,7 +18,7 @@ function DNAinfoDoittMap() {
 	});
 	
 	// add CartoDB tiles
-	this.CartoDBLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+	this.CartoDBLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',{
 	  attribution: 'Created By <a href="http://nijel.org/">NiJeL</a> | &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 	});
 	this.map.addLayer(this.CartoDBLayer);
