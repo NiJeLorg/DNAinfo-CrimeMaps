45205558cfeb7b016a604c1c7d9317aae6ff4bfa
JD Godchaux <jd@nijel.org>
2015-07-28 14:58:01 -0400

    more

---
+++ dnainfo/crimemaps/static/crimemaps/js/chishowMapControllers.js
@@ -0,0 +1,11 @@
+/**
+ * controller.js: listeners and controllers for DNAinfo crime map
+ * Author: NiJeL
+ */
+
+
+$( document ).ready(function() {
+
+    $('.neighborhoodName').text(DNAinfoCHINeighShow.neighborhoodBabyName(neighborhoodLive));
+
+});
