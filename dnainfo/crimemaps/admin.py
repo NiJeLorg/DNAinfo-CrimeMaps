546e019acb21d8db883208e48ffc0eb65e3ccb36
JD Godchaux <jd@nijel.org>
2015-08-10 15:12:29 -0400

    updated admin

--- dnainfo/crimemaps/admin.py
+++ dnainfo/crimemaps/admin.py
@@ -6,3 +6,9 @@ from crimemaps.models import *
 admin.site.register(compstat)
 admin.site.register(doitt)
 admin.site.register(blotter)
+admin.site.register(neighborhoodNYC)
+admin.site.register(neighborhoodCHI)
+admin.site.register(neighborhoodDrawNYC)
+admin.site.register(neighborhoodDrawCHI
+
+
