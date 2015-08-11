b878096b8b617c47cb1b87ca4cf7dd57b88923e4
JD Godchaux <jd@nijel.org>
2015-08-11 17:45:51 -0400

    update

--- dnainfo/crimemaps/models.py
+++ dnainfo/crimemaps/models.py
@@ -124,6 +124,10 @@ class neighborhoodDrawNYC(models.Model):
 	yearsLived = models.IntegerField(default=0, blank=False, null=False)
 	drawnGeojson = models.TextField(default='')
 
+	def __str__(self):
+		return self.neighborhoodLive
+
+
 #model for CHI neighborhood drawing site
 class neighborhoodDrawCHI(models.Model):
 	created = models.DateTimeField(auto_now=True)
@@ -131,6 +135,9 @@ class neighborhoodDrawCHI(models.Model):
 	otherNeighborhood = models.CharField(max_length=255, default='', blank=True, null=True)
 	yearsLived = models.IntegerField(default=0, blank=False, null=False)
 	drawnGeojson = models.TextField(default='')
+
+	def __str__(self):
+		return self.neighborhoodLive
 
 
 # model for Chicago zillow app
