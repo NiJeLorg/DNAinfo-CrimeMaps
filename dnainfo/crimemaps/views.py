df59d0dfc7186881919896a15abc60d93599486d
JD Godchaux <jd@nijel.org>
2015-08-13 17:14:58 -0400

    remove brackets

--- dnainfo/crimemaps/views.py
+++ dnainfo/crimemaps/views.py
@@ -1015,5 +1015,5 @@ def chigeojsonsbyneigh(request, neighborhoodLive=None):
 
 def strip_non_ascii(string):
     ''' Returns the string without non ASCII characters'''
-    stripped = (c for c in string if 0 < ord(c) < 127)
+    stripped = (c for c in string if 0 < ord(c) < 123)
     return ''.join(stripped)
