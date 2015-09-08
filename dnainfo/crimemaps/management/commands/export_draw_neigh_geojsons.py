570d6dbba5054d7fa42d230886c97e0a6922b0e7
JD Godchaux <jd@nijel.org>
2015-09-08 14:41:32 -0400

    update geojson create code

--- dnainfo/crimemaps/management/commands/export_draw_neigh_geojsons.py
+++ dnainfo/crimemaps/management/commands/export_draw_neigh_geojsons.py
@@ -1,4 +1,5 @@
 import sys,os
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from crimemaps.models import *
 
@@ -17,8 +18,8 @@ class Command(BaseCommand):
 	
 	def export_geojsons(self):
 		#set BASE_DIR and make geojson directory if it doesn't exist
-		BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-		geojsonsPath = os.path.join(BASE_DIR, 'geojsons')
+		BASE_DIR = settings.BASE_DIR
+		geojsonsPath = os.path.join(BASE_DIR, 'crimemaps/static/crimemaps/neigh_drawn_geojsons')
 		if not os.path.exists(geojsonsPath):
 			os.makedirs(geojsonsPath)
 
@@ -29,8 +30,7 @@ class Command(BaseCommand):
 			NYCdrawings = neighborhoodDrawNYC.objects.filter(neighborhoodLive=NYCneighborhood).exclude(approved=False)
 			if len(NYCdrawings) > 0:
 				print NYCneighborhood.name	
-				path = 'geojsons/' + NYCneighborhood.dnaurl
-				__location__ = os.path.join(BASE_DIR, path)
+				__location__ = os.path.join(geojsonsPath, NYCneighborhood.dnaurl)
 
 				if not os.path.exists(__location__):
 					os.makedirs(__location__)
@@ -40,6 +40,27 @@ class Command(BaseCommand):
 						filename = NYCneighborhood.dnaurl + "_" + str(count) + ".geojson"
 						with open(os.path.join(__location__, filename), 'wb') as f:
 							changed = NYCdrawing.drawnGeojson.replace('\"properties\":{}', '\"properties\":{\"ID\":\"'+ str(NYCdrawing.id) +'\", \"neighborhoodLive\":\"'+ str(NYCdrawing.neighborhoodLive.name) +'\", \"otherNeighborhood\":\"' + strip_non_ascii(NYCdrawing.otherNeighborhood) +'\", \"yearsLived\":\"' + str(NYCdrawing.yearsLived) +'\"}')
+							f.write(changed)
+							f.close()
+
+		# Pull all neighborhoods CHI
+		CHIneighborhoods = neighborhoodCHI.objects.all()
+
+		for CHIneighborhood in CHIneighborhoods:
+			CHIdrawings = neighborhoodDrawCHI.objects.filter(neighborhoodLive=CHIneighborhood).exclude(approved=False)
+			if len(CHIdrawings) > 0:
+				print CHIneighborhood.name
+				chipath = "chi-" + CHIneighborhood.dnaurl
+				__location__ = os.path.join(geojsonsPath, chipath)
+
+				if not os.path.exists(__location__):
+					os.makedirs(__location__)
+
+				for count, CHIdrawing in enumerate(CHIdrawings):
+					if CHIdrawing.drawnGeojson:
+						filename = CHIneighborhood.dnaurl + "_" + str(count) + ".geojson"
+						with open(os.path.join(__location__, filename), 'wb') as f:
+							changed = CHIdrawing.drawnGeojson.replace('\"properties\":{}', '\"properties\":{\"ID\":\"'+ str(CHIdrawing.id) +'\", \"neighborhoodLive\":\"'+ str(CHIdrawing.neighborhoodLive.name) +'\", \"otherNeighborhood\":\"' + strip_non_ascii(CHIdrawing.otherNeighborhood) +'\", \"yearsLived\":\"' + str(CHIdrawing.yearsLived) +'\"}')
 							f.write(changed)
 							f.close()
 
