053e179016bfda82c3d33bd85f5c1be91ede5ae0
JD Godchaux <jd@nijel.org>
2015-09-14 11:48:14 -0400

    update for Chicago

--- dnainfo/crimemaps/static/crimemaps/js/chiviewMapControllers.js
+++ dnainfo/crimemaps/static/crimemaps/js/chiviewMapControllers.js
@@ -40,13 +40,34 @@ $( document ).ready(function() {
 		$("#legendWrapper").removeClass('hidden');
 		$("#countControls").removeClass('hidden');
 		$("#shapeControls").removeClass('hidden');
+	} else if (countDrawnNeighborhoods > 5) {
+		$( "#all" ).prop('checked', true);
+		$("#shapeControls").removeClass('hidden');
+		$("#drawNeighButton").removeClass('hidden');
 	} else if (countDrawnNeighborhoods > 0) {
 		$( "#all" ).prop('checked', true);
 		$("#shapeControls").removeClass('hidden');
+		$("#drawNeighButton").removeClass('hidden');
 	} else {
 		$("#nobodysDrawn").removeClass('hidden');
-
+		$("#drawNeighButton").removeClass('hidden');
 	}
+
+	// swap out how to use this map text if drawn shapes are less than 25
+	if (countDrawnNeighborhoods < 25) {
+		$("#howToUseThisMap").html("<h4>How to Use This Map:</h4><p>Hover or tap on sections of the map above to see how readers drew " + DNAinfoCHINeighView.neighborhoodBabyName(neighborhood) + ".</p>");
+	}
+
+
+	// expand/contract lower tray
+	$('#popup-expand').click(function() {
+		$( ".popup-wrapper" ).toggleClass("popup-wrapper-open");
+		$( ".popup-wrapper" ).toggleClass("popup-wrapper-75pct");
+		$( ".map" ).toggleClass("map-popup-wrapper-open");
+		$( ".map" ).toggleClass("map-popup-wrapper-75pct");
+		var text = $('#popup-expand').text();
+		$('#popup-expand').text(text == "Expand this window" ? "Contract this window" : "Expand this window");
+	});
 	
 
 
