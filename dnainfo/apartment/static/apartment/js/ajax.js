1a4caee5fb6030486700b62e30ab0e5bd0da2f9e
JD Godchaux <jd@nijel.org>
2016-05-28 00:42:35 -0400

    trying parent.partent

--- dnainfo/apartment/static/apartment/js/ajax.js
+++ dnainfo/apartment/static/apartment/js/ajax.js
@@ -23,7 +23,7 @@ ajaxApplication.firstMove = function () {
 				$('.plusStepper').addClass('disabled');
 				$(".fadein").fadeIn("slow");
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
@@ -62,7 +62,7 @@ ajaxApplication.nextNeighborhood = function () {
 					$('#id_iDontSeeMyNeighborhood').addClass("hidden");
 					$(".fadein").fadeIn("slow");
 					// scroll to top
-					$('body').scrollTo(0);
+					window.parent.parent.scrollTo( 0, 0 );
 
 		        }
 			});
@@ -91,7 +91,7 @@ ajaxApplication.nextLocation = function () {
 					$('#content').html(data);
 					$(".fadein").fadeIn("slow");
 					// scroll to top
-					$('body').scrollTo(0);
+					window.parent.parent.scrollTo( 0, 0 );
 		        }
 			});
 		});
@@ -126,7 +126,7 @@ ajaxApplication.nextYearMoved = function () {
 					$('#id_exactYearMoved').prop("max", 2016);
 					$(".fadein").fadeIn("slow");
 					// scroll to top
-					$('body').scrollTo(0);
+					window.parent.parent.scrollTo( 0, 0 );
 		        }
 			});
 		});
@@ -166,7 +166,7 @@ ajaxApplication.nextBedrooms = function (checked) {
 					$('.minusStepper').addClass('disabled');
 					$(".fadein").fadeIn("slow");
 					// scroll to top
-					$('body').scrollTo(0);
+					window.parent.parent.scrollTo( 0, 0 );
 		        }
 			});
 		});
@@ -204,7 +204,7 @@ ajaxApplication.nextRentSplit = function () {
 					$('.minusStepper').addClass('disabled');
 					$(".fadein").fadeIn("slow");
 					// scroll to top
-					$('body').scrollTo(0);
+					window.parent.parent.scrollTo( 0, 0 );
 		        }
 			});
 		});
@@ -240,7 +240,7 @@ ajaxApplication.nextiPaid = function () {
 					$('#id_iPaid').prop("max", 50000);
 					$(".fadein").fadeIn("slow");
 					// scroll to top
-					$('body').scrollTo(0);
+					window.parent.parent.scrollTo( 0, 0 );
 		        }
 			});
 		});
@@ -272,7 +272,7 @@ ajaxApplication.nextAllPaid = function () {
 					$('#id_allPaid').prop("max", 50000);
 					$(".fadein").fadeIn("slow");
 					// scroll to top
-					$('body').scrollTo(0);
+					window.parent.parent.scrollTo( 0, 0 );
 		        }
 			});
 		});
@@ -299,7 +299,7 @@ ajaxApplication.nextCalc = function () {
 					$('#content').html(data);
 					$(".fadein").fadeIn("slow");
 					// scroll to top
-					$('body').scrollTo(0);
+					window.parent.parent.scrollTo( 0, 0 );
 		        }
 			});
 		});
@@ -320,7 +320,7 @@ ajaxApplication.nextResults = function (id) {
 				$('#content').html(data);
 				$(".fadein").fadeIn("slow");
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
@@ -340,7 +340,7 @@ ajaxApplication.backIntro = function () {
 				$('#content').html(data);
 				$(".fadein").fadeIn("slow");
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
@@ -366,7 +366,7 @@ ajaxApplication.backFirstMove = function (id) {
 				}
 				$(".fadein").fadeIn("slow");
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
@@ -398,7 +398,7 @@ ajaxApplication.backNeighborhood = function (id) {
 				$('#id_iDontSeeMyNeighborhood').addClass("hidden");
 				$(".fadein").fadeIn("slow");    
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
@@ -414,7 +414,7 @@ ajaxApplication.backLocation = function (id) {
 				$('#content').html(data);
 				$(".fadein").fadeIn("slow");
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
@@ -444,7 +444,7 @@ ajaxApplication.backYearMoved = function (id) {
 				}
 				$(".fadein").fadeIn("slow");
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
@@ -472,7 +472,7 @@ ajaxApplication.backBedrooms = function (id) {
 				}
 				$(".fadein").fadeIn("slow");
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
@@ -500,7 +500,7 @@ ajaxApplication.backRentSplit = function (id) {
 				}
 				$(".fadein").fadeIn("slow");
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
@@ -526,7 +526,7 @@ ajaxApplication.backiPaid = function (id) {
 				$('#nextAllPaid').prop("disabled", false);
 				$(".fadein").fadeIn("slow");
 				// scroll to top
-				$('body').scrollTo(0);
+				window.parent.parent.scrollTo( 0, 0 );
 	        }
 		});
 	}
