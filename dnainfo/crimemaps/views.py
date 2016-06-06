3dc3cc0453c981237b303193af83447c6cebb936
JD Godchaux <jd@nijel.org>
2016-06-06 16:44:50 -0400

    next batch

--- dnainfo/crimemaps/views.py
+++ dnainfo/crimemaps/views.py
@@ -871,7 +871,7 @@ def allnycpolygons(request):
 def allnycgeojsons(request):
 
 	#allDrawnNeighborhoods = neighborhoodDrawNYC.objects.all()
-	allDrawnNeighborhoods = neighborhoodDrawNYC.objects.filter(id__gte=27001, id__lte=28000)
+	allDrawnNeighborhoods = neighborhoodDrawNYC.objects.filter(id__gte=28001, id__lte=29000)
 	geojsons = []
 	for drawn in allDrawnNeighborhoods:
 		changed = drawn.drawnGeojson.replace('\"properties\":{}', '\"properties\":{\"ID\":\"'+ str(drawn.id) +'\", \"neighborhoodLive\":\"'+ str(drawn.neighborhoodLive.name) +'\", \"otherNeighborhood\":\"' + strip_non_ascii(drawn.otherNeighborhood) +'\"}')
