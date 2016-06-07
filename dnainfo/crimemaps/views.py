2b1f9f038325e8ef78e8aff41c3d4279d08590d9
JD Godchaux <jd@nijel.org>
2016-06-07 18:24:18 -0400

    next batch

--- dnainfo/crimemaps/views.py
+++ dnainfo/crimemaps/views.py
@@ -1011,7 +1011,7 @@ def allchipolygons(request):
 
 def allchigeojsons(request):
 
-	allDrawnNeighborhoods = neighborhoodDrawCHI.objects.filter(id__gte=9000, id__lte=10000)
+	allDrawnNeighborhoods = neighborhoodDrawCHI.objects.filter(id__gte=10000, id__lte=11000)
 	geojsons = []
 	for drawn in allDrawnNeighborhoods:
 		changed = drawn.drawnGeojson.replace('\"properties\":{}', '\"properties\":{\"ID\":\"'+ str(drawn.id) +'\", \"neighborhoodLive\":\"'+ str(drawn.neighborhoodLive.name) +'\", \"otherNeighborhood\":\"' + strip_non_ascii(drawn.otherNeighborhood) +'\"}')
