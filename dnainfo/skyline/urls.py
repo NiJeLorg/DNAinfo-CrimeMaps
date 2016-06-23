e44f808f3c4b35d912d815a4ef7a099710a55fbf
JD Godchaux <jd@nijel.org>
2016-06-23 18:39:19 -0400

    updating skyline namespace so it doesn't conflict

--- dnainfo/skyline/urls.py
+++ dnainfo/skyline/urls.py
@@ -5,14 +5,14 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^skyline/admin/$', views.skylineAdmin, name='skylineAdmin'),
     url(r'^skyline/admin/next/$', views.skylineAdminNext, name='skylineAdminNext'),
-    url(r'^skyline/nyc/$', views.intro, name='intro'),
-    url(r'^skyline/nyc/whatNeighborhood/$', views.whatNeighborhood, name='whatNeighborhood'),
-    url(r'^skyline/nyc/whatNeighborhood/(?P<id>\d+)/$', views.whatNeighborhood, name='whatNeighborhood'),
-    url(r'^skyline/nyc/buildingHeight/(?P<id>\d+)/$', views.buildingHeight, name='buildingHeight'),
-    url(r'^skyline/nyc/exactLocation/(?P<id>\d+)/$', views.exactLocation, name='exactLocation'),
-    url(r'^skyline/nyc/getGeojson/(?P<id>\d+)/$', views.getGeojson, name='getGeojson'),
-    url(r'^skyline/nyc/end/(?P<id>\d+)/$', views.end, name='end'),
-    url(r'^skyline/nyc/results/(?P<id>\d+)/$', views.results, name='results'),
+    url(r'^skyline/nyc/$', views.skyline_intro, name='skyline_intro'),
+    url(r'^skyline/nyc/whatNeighborhood/$', views.skyline_whatNeighborhood, name='skyline_whatNeighborhood'),
+    url(r'^skyline/nyc/whatNeighborhood/(?P<id>\d+)/$', views.skyline_whatNeighborhood, name='skyline_whatNeighborhood'),
+    url(r'^skyline/nyc/buildingHeight/(?P<id>\d+)/$', views.skyline_buildingHeight, name='skyline_buildingHeight'),
+    url(r'^skyline/nyc/exactLocation/(?P<id>\d+)/$', views.skyline_exactLocation, name='skyline_exactLocation'),
+    url(r'^skyline/nyc/getGeojson/(?P<id>\d+)/$', views.skyline_getGeojson, name='skyline_getGeojson'),
+    url(r'^skyline/nyc/end/(?P<id>\d+)/$', views.skyline_end, name='skyline_end'),
+    url(r'^skyline/nyc/results/(?P<id>\d+)/$', views.skyline_results, name='skyline_results'),
 
 ]
 
