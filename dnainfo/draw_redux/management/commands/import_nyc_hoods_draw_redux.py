f9dab9ca394b0691e9e503235cd33880b906521b
JD Godchaux <jd@nijel.org>
2016-06-29 19:18:49 -0400

    model change for neighborhoodNYC_redux

--- dnainfo/draw_redux/management/commands/import_nyc_hoods_draw_redux.py
+++ dnainfo/draw_redux/management/commands/import_nyc_hoods_draw_redux.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     def load_nyc_hoods_data(self):
         #truncate NYCmyFirstApartment, which has a foreign key on neighborhoodNYC
         NYCInOrOut.objects.all().delete()
-        neighborhoodNYC.objects.all().delete()
+        neighborhoodNYC_redux.objects.all().delete()
         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         # open LIS_Beacon_Beach_WQdata.csv and dump into BeachWQSamples table
         with open(os.path.join(__location__, 'nyc_hoods.csv'), 'rb') as f:
