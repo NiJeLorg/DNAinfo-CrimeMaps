93772f92978368e8ab20fa206e9489b1bd609957
JD Godchaux <jd@nijel.org>
2017-01-21 07:33:11 -0500

    updating admin area tables

--- dnainfo/skyline/admin.py
+++ dnainfo/skyline/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from skyline.models import *
 
+admin.site.register(neighborhoodNYC)
 admin.site.register(NYCskyline)
 admin.site.register(NYCSponsoredBuildings)
 admin.site.register(NYCReporterBuildings)
 admin.site.register(NYC_DOB_Permit_Issuance)
+
 
